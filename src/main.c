8ff4165f87c97e2c712f2102d7a41379e2b99feb
Kevin Blanchard <blanchardkevinj@gmail.com>
2025-03-15 14:13:32 -0400

    update main, add test lua script

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <Supergoon/engine.h>
+#include <Supergoon/window.h>
 #include <stdio.h>
 static void Update(void) {
 	puts("Hello world!");
 }
 
 int main(void) {
+	SetWindowOptions(640, 480, "Escape The Fate");
 	SetUpdateFunction(&Update);
 	Run();
 }
