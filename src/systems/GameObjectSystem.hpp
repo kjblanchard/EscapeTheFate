6b79b7572c93d6f9b977e9961a9846f6f35d7860
Kevin Blanchard <kevin@supergoon.com>
2026-07-06 17:58:13 -0400

    refac gameobjectsystem

--- src/systems/GameObjectSystem.hpp
+++ src/systems/GameObjectSystem.hpp
@@ -5,31 +5,31 @@
 struct TiledObject;
 namespace Etf {
 class GameObject;
-	void StartGameObjectSystem();
-	void LoadGameObjectSystem();
-	void UpdateGameObjectSystem();
-	void DrawGameObjectSystem();
-	// Add a gameobject to be managed by the gameobject system
-	void AddGameObject(GameObject* gameobject);
-	void ShutdownGameObjectSystem();
+void StartGameObjectSystem();
+void LoadGameObjectSystem();
+void UpdateGameObjectSystem();
+void DrawGameObjectSystem();
+// Add a gameobject to be managed by the gameobject system
+void AddGameObject(GameObject* gameobject);
+void ShutdownGameObjectSystem();
 
-	// Allows you go get a list of specific gameobjects, if you use specific interfaces on them, useful for querying
-	template <typename T>
-	static std::vector<T*>& GetGameObjectsOfType() {
-		static std::vector<T*> list;
-		return list;
-	}
+// Allows you go get a list of specific gameobjects, if you use specific interfaces on them, useful for querying
+template <typename T>
+std::vector<T*>& GetGameObjectsOfType() {
+	static std::vector<T*> list;
+	return list;
+}
 
-	template <typename T>
-	static void AddGameObjectOfType(T* gameobject) {
-		auto& list = GetGameObjectsOfType<T>();
-		list.push_back(gameobject);
-	}
+template <typename T>
+void AddGameObjectOfType(T* gameobject) {
+	auto& list = GetGameObjectsOfType<T>();
+	list.push_back(gameobject);
+}
 
-	// Does not destroy the gameobject, but it does remove it from the vector it's tracked in
-	template <typename T>
-	static void RemoveGameObjectOfType(T* gameobject) {
-		auto& list = GetGameObjectsOfType<T>();
-		list.erase(std::remove(list.begin(), list.end(), gameobject), list.end());
-	}
+// Does not destroy the gameobject, but it does remove it from the vector it's tracked in
+template <typename T>
+void RemoveGameObjectOfType(T* gameobject) {
+	auto& list = GetGameObjectsOfType<T>();
+	list.erase(std::remove(list.begin(), list.end(), gameobject), list.end());
+}
 }  // namespace Etf
