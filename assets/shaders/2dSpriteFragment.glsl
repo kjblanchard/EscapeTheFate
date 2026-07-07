2849ebe16d44fecd9cea7e314c19ebd1055bb1e6
Kevin.Blanchard <kevin.blanchard@ally.com>
2026-07-06 22:39:39 -0400

    try it

--- assets/shaders/2dSpriteFragment.glsl
+++ assets/shaders/2dSpriteFragment.glsl
@@ -4,10 +4,14 @@ out vec4 color;
 
 uniform sampler2D image;
 uniform vec4 spriteColor;
+uniform vec4 srcRect;
 uniform vec2 textureSize;
 
 void main()
 {
     ivec2 texel = ivec2(TexCoords * textureSize);
+    ivec2 minTexel = ivec2(srcRect.xy);
+    ivec2 maxTexel = ivec2(srcRect.xy + srcRect.zw) - 1;
+    texel = clamp(texel, minTexel, maxTexel);
     color = spriteColor * texelFetch(image, texel, 0);
 }
--- assets/shaders/2dSpriteFragmentE.glsl
+++ assets/shaders/2dSpriteFragmentE.glsl
@@ -6,10 +6,14 @@ out vec4 FragColor;
 
 uniform sampler2D image;
 uniform vec4 spriteColor;
+uniform vec4 srcRect;
 uniform vec2 textureSize;
 
 void main()
 {
     ivec2 texel = ivec2(TexCoords * textureSize);
+    ivec2 minTexel = ivec2(srcRect.xy);
+    ivec2 maxTexel = ivec2(srcRect.xy + srcRect.zw) - 1;
+    texel = clamp(texel, minTexel, maxTexel);
     FragColor = spriteColor * texelFetch(image, texel, 0);
 }
