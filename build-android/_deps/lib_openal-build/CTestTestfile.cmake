e36dc43711ca1d82fe5b3270bf2f5e1965373f31
Kevin Blanchard <kevin@supergoon.com>
2026-08-27 21:19:10 -0400

    Stop tracking build-android

--- build-android/_deps/lib_openal-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kevin/git/cpp/EscapeTheFate/build-android/_deps/lib_openal-src
-# Build directory: /Users/kevin/git/cpp/EscapeTheFate/build-android/_deps/lib_openal-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("fmt-11.1.1")
--- build-android/_deps/lib_openal-build/fmt-11.1.1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kevin/git/cpp/EscapeTheFate/build-android/_deps/lib_openal-src/fmt-11.1.1
-# Build directory: /Users/kevin/git/cpp/EscapeTheFate/build-android/_deps/lib_openal-build/fmt-11.1.1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
